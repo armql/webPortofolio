1995284b0607f196362112d3f3ee3010128e5d2a
am <arlindmaliqi28@gmail.com>
2023-12-12 21:08:21 +0100

    better router with guest layout

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,8 +1,35 @@
-import { createBrowserRouter } from "react-router-dom";
-import { LazyLoader, NotFound } from "./routes/global/global";
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import {
+  LazyLoader,
+  NotFound,
+  GuestLayout,
+  Home,
+} from "./routes/global/global";
 import { Suspense } from "react";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={<LazyLoader />}>
+        <GuestLayout />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="home" />,
+      },
+      {
+        path: "home",
+        element: (
+          <Suspense fallback={<LazyLoader />}>
+            <Home />
+          </Suspense>
+        ),
+      },
+    ],
+  },
   {
     path: "*",
     element: (
--- src/router/routes/global/global.js
+++ src/router/routes/global/global.js
@@ -2,4 +2,8 @@ import { lazy } from "react";
 
 import LazyLoader from "../loader/LazyLoader";
 const NotFound = lazy(() => import("../../../layouts/NotFound/NotFound"));
-export { NotFound, LazyLoader };
+const Home = lazy(() => import("../../../views/Home/Home"));
+const GuestLayout = lazy(
+  () => import("../../../layouts/GuestLayout/GuestLayout"),
+);
+export { NotFound, LazyLoader, GuestLayout, Home };
