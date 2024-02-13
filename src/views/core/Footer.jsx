a37330a66c912109d78aa2830e3f2bf5de9f90fd
am <arlindmaliqi28@gmail.com>
2024-02-13 17:23:10 +0100

    links added

--- src/views/core/Footer.jsx
+++ src/views/core/Footer.jsx
@@ -3,10 +3,13 @@ export default function Footer() {
     <footer className="flex w-full items-center justify-between px-6 py-8">
       <span className="font-medium">armlq28</span>
       <div className="flex flex-row items-center gap-4 font-light">
-        <a href="#" className="hover:underline">
+        <a
+          href="https://www.linkedin.com/in/arlind-m-54424721a?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAADc1bkQBouoNS7-fZrVbA_VCq96t74iiris&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_people%3Bejb4qlidT%2B6s331sfWuiaw%3D%3D"
+          className="hover:underline"
+        >
           Linkedin
         </a>
-        <a href="#" className="hover:underline">
+        <a href="https://github.com/armql" className="hover:underline">
           Github
         </a>
       </div>
