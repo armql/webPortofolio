f440c6164bebe66d9d9e4808a04c04f7f71086f9
am <arlindmaliqi28@gmail.com>
2024-02-13 17:18:56 +0100

    finalized footer

---
+++ src/views/core/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="flex w-full items-center justify-between px-6 py-8">
+      <span className="font-medium">armlq28</span>
+      <div className="flex flex-row items-center gap-4 font-light">
+        <a href="#" className="hover:underline">
+          Linkedin
+        </a>
+        <a href="#" className="hover:underline">
+          Github
+        </a>
+      </div>
+    </footer>
+  );
+}
